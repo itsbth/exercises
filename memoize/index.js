6d3ba0a4c517d5cea55ecba9a5e0f5967237086c
Bjørn Tore Håvie <bjorn.haavie@inonit.no>
2015-06-17 14:54:12 +0200

    memoize solution.

---
+++ memoize/index.js
@@ -0,0 +1,10 @@
+const NOVAL = Symbol('NOVAL');
+
+module.exports = function (fn) {
+    const memo = new Map();
+    return function () {
+        const args = [].slice.call(arguments),
+              key = JSON.stringify(args);
+        return memo.has(key) ? memo.get(key) : memo.set(key, fn.apply(this, args)).get(key);
+    };
+};
